8b1d3f28609db043d414e85ccb4e5c0f63c1d163
jyjh <jerryyenjh@gmail.com>
2026-03-19 02:03:29 +0800

    Update HelpWindow to use the correct USERGUIDE_URL

--- src/main/java/seedu/address/ui/HelpWindow.java
+++ src/main/java/seedu/address/ui/HelpWindow.java
@@ -16,7 +16,7 @@ import seedu.address.logic.commands.CommandRegistry;
  */
 public class HelpWindow extends UiPart<Stage> {
 
-    public static final String USERGUIDE_URL = "https://se-education.org/addressbook-level3/UserGuide.html";
+    public static final String USERGUIDE_URL = "https://ay2526s2-cs2103t-w09-2.github.io/tp/UserGuide.html";
     public static final String HELP_MESSAGE = "Refer to the user guide: " + USERGUIDE_URL;
 
     private static final Logger logger = LogsCenter.getLogger(HelpWindow.class);
